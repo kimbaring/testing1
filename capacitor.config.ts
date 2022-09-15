753a21b0ef4022cb52eea90cf944725a9cca155d
nikko <jtapayan@proweaver.net>
2022-09-15 22:44:37 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'push-notif',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
